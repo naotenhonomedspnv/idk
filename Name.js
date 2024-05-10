64391a88d662ba925c321c61ed15889089e339f3
naotenhonomedspnv <130319141+naotenhonomedspnv@users.noreply.github.com>
2024-05-10 20:26:11 -0300

    Update and rename Name.js to Name.html

--- Name.js
+++
@@ -1,8 +0,0 @@
-fetch('https://rawcdn.githack.com/naotenhonomedspnv/idk/d57de3b5dc39cae3c716b96c4650e7afc6f5a023/main.html')
-    .then(response => response.text())
-    .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const value = doc.getElementById('valueContainer').innerText;
-        console.log(value); // This will log "Hello, world!" in the console
-    });
